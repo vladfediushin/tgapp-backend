f8849910ea58eb387b426d1ef23914758d7f34d6
Vlad Fediushin <vladfedyushin@gmail.com>
2025-07-13 00:54:03 +0400

    create_async_engine modified (set pool_size, pool_overflow, pool_timeout)

--- app/database.py
+++ app/database.py
@@ -8,7 +8,12 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://")
 
-engine = create_async_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
+engine = create_async_engine(DATABASE_URL, 
+                             echo=True, 
+                             pool_size=5,
+                             max_overflow=5,
+                             pool_timeout=30,
+                             pool_pre_ping=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 Base = declarative_base()
 
