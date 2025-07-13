b72d2716635827e92971a50943ed90c26c6a684b
Vlad Fediushin <vladfedyushin@gmail.com>
2025-07-13 17:41:50 +0400

    logging

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-base_url = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://")
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://")
 
 connect_args = {
     "statement_cache_size": 0,
@@ -22,15 +22,16 @@ connect_args = {
 logger.info(f"Creating engine with connect_args: {connect_args}")
 logger.info(f"Database URL (masked): {DATABASE_URL.split('@')[0]}@[MASKED]")
 
-if "?" in base_url:
-    DATABASE_URL = f"{base_url}&statement_cache_size=0&prepared_statement_cache_size=0"
-else:
-    DATABASE_URL = f"{base_url}?statement_cache_size=0&prepared_statement_cache_size=0"
-
 
 engine = create_async_engine(DATABASE_URL, 
                              echo=True, 
-                             pool_pre_ping=True
+                             connect_args={
+                            "statement_cache_size": 0,           
+                            "prepared_statement_cache_size": 0,  
+                            "server_settings": {
+                                "application_name": "fastapi_app",  
+                            }
+                        },
                             )
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 Base = declarative_base()
