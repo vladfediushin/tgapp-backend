3869829b524a44921d2a9f41a438c3f6b97412b5
Vlad Fediushin <vladfedyushin@gmail.com>
2025-11-11 16:37:44 +0400

    Use DefaultBotProperties for HTML parse mode

--- bot/wiring.py
+++ bot/wiring.py
@@ -2,6 +2,7 @@
 import os
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 from .handlers import router as handlers_router
 
@@ -9,7 +10,7 @@ from .handlers import router as handlers_router
 def get_bot_and_dispatcher():
     """Instantiate aiogram Bot and Dispatcher and register handlers."""
     token = os.environ["TELEGRAM_BOT_TOKEN"]
-    bot = Bot(token, parse_mode=ParseMode.HTML)
+    bot = Bot(token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
     dp.include_router(handlers_router)
     return bot, dp
