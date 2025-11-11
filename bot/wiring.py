20d8aa10fec4190ad026d75814f030fd7a3d78a1
Vlad Fediushin <vladfedyushin@gmail.com>
2025-11-11 16:26:35 +0400

    Enable HTML parse mode for bot responses

--- bot/wiring.py
+++ bot/wiring.py
@@ -1,6 +1,7 @@
 """Bot/dispatcher wiring helpers."""
 import os
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 
 from .handlers import router as handlers_router
 
@@ -8,7 +9,7 @@ from .handlers import router as handlers_router
 def get_bot_and_dispatcher():
     """Instantiate aiogram Bot and Dispatcher and register handlers."""
     token = os.environ["TELEGRAM_BOT_TOKEN"]
-    bot = Bot(token)
+    bot = Bot(token, parse_mode=ParseMode.HTML)
     dp = Dispatcher()
     dp.include_router(handlers_router)
     return bot, dp
